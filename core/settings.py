4f652185628d1f2dca15144e78d375ffd12e7409
David Castagneto <davidcastagnetoa@gmail.com>
2024-05-26 03:06:44 +0200

    new branch

--- core/settings.py
+++ core/settings.py
@@ -82,12 +82,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
-# Database
-DATABASES = {
-    "default": env.db("DATABASE_URL", default="postgres:///ninerogues"),  # Cambiar por el nombre de tu DB. "username:///dbname"
-}
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# Aun no tienes DB en  esta maquina ni en AWS
+# # Database
+# DATABASES = {
+#     "default": env.db("DATABASE_URL", default="postgres:///ninerogues"),  # Cambiar por el nombre de tu DB. "username:///dbname"
+# }
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
 # Urls Autorizathed (En produccion nombre del dominio real)
@@ -172,83 +172,83 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
-SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('JWT', ),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10080),
-    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
-    'ROTATE_REFRESFH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'AUTH_TOKEN_CLASSES': (
-        'rest_framework_simplejwt.tokens.AccessToken',
-    )
-}
+# SIMPLE_JWT = {
+#     'AUTH_HEADER_TYPES': ('JWT', ),
+#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10080),
+#     # 'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+#     'ROTATE_REFRESFH_TOKENS': True,
+#     'BLACKLIST_AFTER_ROTATION': True,
+#     'AUTH_TOKEN_CLASSES': (
+#         'rest_framework_simplejwt.tokens.AccessToken',
+#     )
+# }
 
-DJOSER = {
-    'LOGIN_FIELD': 'email',
-    'USER_CREATE_PASSWORD_RETYPE': True,
-    'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
-    'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
-    'SEND_CONFIRMATION_EMAIL': True,
-    'SET_USERNAME_RETYPE': True,
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'SET_PASSWORD_RETYPE': True,
-    'PASSWORD_RESET_CONFIRM_RETYPE': True,
-    'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': 'activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
-    'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
-    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:8000/google', 'http://localhost:8000/facebook'],
-    'SERIALIZERS': {
-        'user_create': 'apps.user.serializers.UserCreateSerializer',
-        'user': 'apps.user.serializers.UserCreateSerializer',
-        'current_user': 'apps.user.serializers.UserCreateSerializer',
-        'user_delete': 'djoser.serializers.UserDeleteSerializer',
-    },
-}
+# DJOSER = {
+#     'LOGIN_FIELD': 'email',
+#     'USER_CREATE_PASSWORD_RETYPE': True,
+#     'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
+#     'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
+#     'SEND_CONFIRMATION_EMAIL': True,
+#     'SET_USERNAME_RETYPE': True,
+#     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+#     'SET_PASSWORD_RETYPE': True,
+#     'PASSWORD_RESET_CONFIRM_RETYPE': True,
+#     'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
+#     'ACTIVATION_URL': 'activate/{uid}/{token}',
+#     'SEND_ACTIVATION_EMAIL': True,
+#     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
+#     'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:8000/google', 'http://localhost:8000/facebook'],
+#     'SERIALIZERS': {
+#         'user_create': 'apps.user.serializers.UserCreateSerializer',
+#         'user': 'apps.user.serializers.UserCreateSerializer',
+#         'current_user': 'apps.user.serializers.UserCreateSerializer',
+#         'user_delete': 'djoser.serializers.UserDeleteSerializer',
+#     },
+# }
 
-BT_ENVIRONMENT = os.environ.get('BT_ENVIRONMENT')
-BT_MERCHANT_ID = os.environ.get('BT_MERCHANT_ID')
-BT_PUBLIC_KEY = os.environ.get('BT_PUBLIC_KEY')
-BT_PRIVATE_KEY = os.environ.get('BT_PRIVATE_KEY')
-
-
-AUTH_USER_MODEL = "user.UserAccount"
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# BT_ENVIRONMENT = os.environ.get('BT_ENVIRONMENT')
+# BT_MERCHANT_ID = os.environ.get('BT_MERCHANT_ID')
+# BT_PUBLIC_KEY = os.environ.get('BT_PUBLIC_KEY')
+# BT_PRIVATE_KEY = os.environ.get('BT_PRIVATE_KEY')
 
 
-if not DEBUG:
-    DEFAULT_FROM_EMAIL = 'Vudera - Academia de Software <mail@vudera.com>'
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+# AUTH_USER_MODEL = "user.UserAccount"
 
-    # # django-ckeditor will not work with S3 through django-storages without this line in settings.py
-    # AWS_QUERYSTRING_AUTH = False
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-    # # aws settings
 
-    # AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-    # AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-    # AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+# if not DEBUG:
+#     DEFAULT_FROM_EMAIL = 'Vudera - Academia de Software <mail@vudera.com>'
+#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#     EMAIL_HOST = env('EMAIL_HOST')
+#     EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+#     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+#     EMAIL_PORT = env('EMAIL_PORT')
+#     EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 
-    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    # AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    # AWS_DEFAULT_ACL = 'public-read'
+# # django-ckeditor will not work with S3 through django-storages without this line in settings.py
+# AWS_QUERYSTRING_AUTH = False
 
-    # # s3 static settings
-    # STATIC_LOCATION = 'static'
-    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# # aws settings
 
-    # # s3 public media settings
+# AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
-    # PUBLIC_MEDIA_LOCATION = 'media'
-    # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    # DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStore'
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# AWS_DEFAULT_ACL = 'public-read'
 
-    # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),)
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# # s3 static settings
+# STATIC_LOCATION = 'static'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# # s3 public media settings
+
+# PUBLIC_MEDIA_LOCATION = 'media'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+# DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStore'
+
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
